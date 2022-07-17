e615d2bd700b4f48eecc2ef95c8ca89a5f43d364
John Moberg <john@depict.ai>
2022-07-17 22:48:04 +0200

    Day 4, part 2

--- 2015/day4/src/main.rs
+++ 2015/day4/src/main.rs
@@ -2,15 +2,20 @@ use md5;
 
 fn main() {
     let input = &String::from("bgvyzdsv");
-    println!("Part 1: n = {}", lowest_n(input));
+    println!("Part 1: n = {}", lowest_n(input, 5));
+    println!("Part 2: n = {}", lowest_n(input, 6));
 }
 
-fn lowest_n(key: &String) -> u64 {
+fn lowest_n(key: &String, n_zeros: usize) -> u64 {
     let mut i = 0u64;
     loop {
+        if i % 10000 == 0 {
+            println!("Checked {} hashes", i);
+        }
+
         let hash = format!("{:x}", md5::compute(key.to_owned() + &(i.to_string())[..]));
 
-        if &hash[..5] == "00000" {
+        if &hash[..n_zeros] == "0".repeat(n_zeros) {
             return i;
         }
 
@@ -20,6 +25,6 @@ fn lowest_n(key: &String) -> u64 {
 
 #[test]
 fn test_lowest_n() {
-    assert_eq!(lowest_n(&String::from("abcdef")), 609043);
-    assert_eq!(lowest_n(&String::from("pqrstuv")), 1048970);
+    assert_eq!(lowest_n(&String::from("abcdef"), 5), 609043);
+    assert_eq!(lowest_n(&String::from("pqrstuv"), 5), 1048970);
 }
