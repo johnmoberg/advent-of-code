ab171088c21ec4cfab87c72a369197cdc9cd5c52
John Moberg <john@depict.ai>
2022-07-17 21:22:37 +0200

    Day 1, part 2

--- 2015/day1/src/main.rs
+++ 2015/day1/src/main.rs
@@ -1,12 +1,16 @@
 use std::fs;
 
 fn main() {
-    println!("{}", get_floor(fs::read_to_string("input.txt").unwrap()));
+    println!("Ended up on floor {}", floor(fs::read_to_string("input.txt").unwrap()));
 
 }
 
-fn get_floor(instructions: String) -> i64 {
-    instructions.chars().fold(0, |f, p| {
+fn floor(instructions: String) -> i64 {
+    instructions.chars().enumerate().fold(0, |f, (i, p)| {
+        if f == -1 {
+            println!("In basement at position {}", i)
+        }
+
         f + match p {
             '(' => 1,
             ')' => -1,
@@ -17,8 +21,8 @@ fn get_floor(instructions: String) -> i64 {
 
 #[test]
 fn test_get_floor() {
-    assert_eq!(get_floor(String::from("(())")), 0);
-    assert_eq!(get_floor(String::from("(((")), 3);
-    assert_eq!(get_floor(String::from("())")), -1);
-    assert_eq!(get_floor(String::from(")())())")), -3);
+    assert_eq!(floor(String::from("(())")), 0);
+    assert_eq!(floor(String::from("(((")), 3);
+    assert_eq!(floor(String::from("())")), -1);
+    assert_eq!(floor(String::from(")())())")), -3);
 }
