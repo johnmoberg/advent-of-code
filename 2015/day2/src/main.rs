60c400e52483dffbb7364190106a38d2590a5620
John Moberg <john@depict.ai>
2022-07-17 21:53:23 +0200

    Day 2, part 2

--- 2015/day2/src/main.rs
+++ 2015/day2/src/main.rs
@@ -5,28 +5,52 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let total = reader
-        .lines()
-        .fold(0, |sum, val| sum + required_wrapping(val.unwrap()));
-    println!("Need {} wrapping", total);
+    let total = reader.lines().fold((0, 0), |sum, val| {
+        let v = val.unwrap();
+        (sum.0 + required_wrapping(&v), sum.1 + required_ribbon(&v))
+    });
+    println!("Need {} wrapping and {} ribbon", total.0, total.1);
 }
 
-fn required_wrapping(dimensions: String) -> u64 {
-    let dims = dimensions
+fn dimensions(line: &String) -> Option<(u64, u64, u64)> {
+    let dims = line
         .split('x')
         .map(|x| x.parse::<u64>().unwrap())
         .collect::<Vec<u64>>();
 
     if let [l, w, h] = dims[..] {
-        let slack = *vec![l * w, w * h, h * l].iter().min().unwrap();
-        2 * (l * w + w * h + h * l) + slack
+        Some((l, w, h))
     } else {
-        panic!("Unexpected input")
+        None
     }
 }
 
+fn required_wrapping(line: &String) -> u64 {
+    let (l, w, h) = dimensions(line).unwrap();
+    let slack = *vec![l * w, w * h, h * l].iter().min().unwrap();
+
+    2 * (l * w + w * h + h * l) + slack
+}
+
+fn required_ribbon(line: &String) -> u64 {
+    let (l, w, h) = dimensions(line).unwrap();
+    let min_perimeter = *vec![2 * (l + w), 2 * (l + h), 2 * (w + h)]
+        .iter()
+        .min()
+        .unwrap();
+    let volume = l * w * h;
+
+    min_perimeter + volume
+}
+
 #[test]
-fn test_get_floor() {
-    assert_eq!(required_wrapping(String::from("2x3x4")), 58);
-    assert_eq!(required_wrapping(String::from("1x1x10")), 43);
+fn test_p1() {
+    assert_eq!(required_wrapping(&String::from("2x3x4")), 58);
+    assert_eq!(required_wrapping(&String::from("1x1x10")), 43);
+}
+
+#[test]
+fn test_p2() {
+    assert_eq!(required_ribbon(&String::from("2x3x4")), 34);
+    assert_eq!(required_ribbon(&String::from("1x1x10")), 14);
 }
