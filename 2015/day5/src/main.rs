5647026ca77fe5e02e32054f0658cff92efd1468
John Moberg <john@depict.ai>
2022-07-18 23:43:02 +0200

    Day 5, part 2

--- 2015/day5/src/main.rs
+++ 2015/day5/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -8,7 +9,7 @@ fn main() {
     let reader = BufReader::new(file);
     let n_nice = reader
         .lines()
-        .fold(0, |n, s| n + (nice_string(&s.unwrap()[..]) as i64));
+        .fold(0, |n, s| n + (nice_string_2(&s.unwrap()[..]) as i64));
     println!("Got {} nice strings", n_nice);
 }
 
@@ -38,7 +39,37 @@ fn nice_string(input: &str) -> bool {
         }
     }
 
-    return vowels >= 3 && twice_in_a_row;
+    vowels >= 3 && twice_in_a_row
+}
+
+fn nice_string_2(input: &str) -> bool {
+    let vec_input = input.chars().collect::<Vec<char>>();
+
+    let mut map = HashMap::new();
+    let mut maybe_last_s: Option<String> = None;
+
+    for slice in vec_input.windows(2) {
+        let (a, b) = (slice[0], slice[1]);
+        let s = [a, b].iter().collect::<String>();
+
+        if let Some(last_s) = maybe_last_s {
+            if last_s == s {
+                maybe_last_s = None;
+                continue;
+            } else {
+                maybe_last_s = Some(s.clone());
+                *map.entry(s).or_insert(0) += 1;
+            }
+        } else {
+            maybe_last_s = Some(s.clone());
+            *map.entry(s).or_insert(0) += 1;
+        }
+    }
+
+    let pairs = map.into_iter().any(|(_, v)| v >= 2);
+    let repeat = vec_input.windows(3).any(|s| s[0] == s[2]);
+
+    pairs && repeat
 }
 
 #[test]
@@ -48,4 +79,12 @@ fn test_nice_string() {
     assert_eq!(nice_string("jchzalrnumimnmhp"), false);
     assert_eq!(nice_string("haegwjzuvuyypxyu"), false);
     assert_eq!(nice_string("dvszwmarrgswjxmb"), false);
+}
+
+#[test]
+fn test_nice_string_2() {
+    assert_eq!(nice_string_2("qjhvhtzxzqqjkmpb"), true);
+    assert_eq!(nice_string_2("xxyxx"), true);
+    assert_eq!(nice_string_2("uurcxstgmygtbstg"), false);
+    assert_eq!(nice_string_2("ieodomkazucvgmuy"), false);
 }
