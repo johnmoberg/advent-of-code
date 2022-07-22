690885157e9f49ad54607ab1caa8a94e5dd73db7
John Moberg <john@depict.ai>
2022-07-22 22:36:19 +0200

    Day 13, part 2

--- 2015/day13/src/main.rs
+++ 2015/day13/src/main.rs
@@ -10,10 +10,18 @@ use regex::Regex;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let lines = input.split("\n").collect::<Vec<&str>>();
-    let (people, map) = parse_input(&lines);
+    let (mut people, mut map) = parse_input(&lines);
 
     let answer = part1(&people, &map);
     println!("Part 1: {}", answer);
+
+    for person in &people {
+        map.insert((String::from("John"), person.clone()), 0);
+        map.insert((person.clone(), String::from("John")), 0);
+    }
+    people.push(String::from("John"));
+    let answer = part1(&people, &map);
+    println!("Part 2: {}", answer);
 }
 
 fn part1(people: &Vec<String>, map: &HashMap<(String, String), i64>) -> i64 {
