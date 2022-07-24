9f2be5f8b81e39ff04ff0a3cb78f2f69c44191d1
John Moberg <john@depict.ai>
2022-07-24 12:02:33 +0200

    Day 17, part 2

--- 2015/day17/src/main.rs
+++ 2015/day17/src/main.rs
@@ -1,20 +1,40 @@
 use itertools::Itertools;
+use std::cmp;
 
 fn main() {
-    let target = 150u64;
     let containers: Vec<u64> = vec![
         43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34, 17, 17, 44, 36, 31, 46, 9, 27, 38,
     ];
+    let target = 150u64;
 
+    let (min_containers, max_containers) = (1, containers.len() as u64);
+    let (n_combinations, min_needed) = part1(target, min_containers, max_containers, &containers);
+    println!("Part 1: {}", n_combinations);
+
+    let (n_combinations, _) = part1(target, min_needed, min_needed, &containers);
+    println!("Part 2: {}", n_combinations);
+}
+
+fn part1(
+    target: u64,
+    min_containers: u64,
+    max_containers: u64,
+    containers: &Vec<u64>,
+) -> (u64, u64) {
     let mut n_combinations = 0;
+    let mut min_needed = u64::MAX;
 
-    for n_containers in 1..=containers.len() {
-        for combination in containers.iter().combinations(n_containers) {
+    for n_containers in min_containers..=max_containers {
+        for combination in containers
+            .iter()
+            .combinations(n_containers.try_into().unwrap())
+        {
             if combination.iter().copied().sum::<u64>() == target {
                 n_combinations += 1;
+                min_needed = cmp::min(min_needed, n_containers);
             }
         }
     }
 
-    println!("Part 1: {}", n_combinations);
+    (n_combinations, min_needed)
 }
